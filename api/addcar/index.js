151598c45dc13035b0ee4da5854c9fc003626378
unknown <anistplenitud@gmail.com>
2024-04-10 23:50:11 +0200

    more fixes3

--- api/addcar/index.js
+++ api/addcar/index.js
@@ -3,7 +3,29 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
 
-    cars = require('../../src/cars.json');
+    cars = [
+        {
+            "id":"1",
+            "make": "Toyota",
+            "model": "Camry",
+            "year": 2022,
+            "price": 250000
+        },
+        {
+            "id":"2",
+            "make": "Honda",
+            "model": "Accord",
+            "year": 2021,
+            "price": 200000
+        },
+        {
+            "id":"3",
+            "make": "Ford",
+            "model": "Mustang",
+            "year": 2020,
+            "price": 300000
+        }
+    ];
 
     const newCar = req.body;
 
--- api/cars/index.js
+++ api/cars/index.js
@@ -1,10 +1,34 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    cars = require('../../src/cars.json');
+    const cars = [
+        {
+            "id":"1",
+            "make": "Toyota",
+            "model": "Camry",
+            "year": 2022,
+            "price": 250000
+        },
+        {
+            "id":"2",
+            "make": "Honda",
+            "model": "Accord",
+            "year": 2021,
+            "price": 200000
+        },
+        {
+            "id":"3",
+            "make": "Ford",
+            "model": "Mustang",
+            "year": 2020,
+            "price": 300000
+        }
+    ];
+
+    const NodeCache = require( "node-cache" );
 
     context.res = {
-        status: 200, /* Defaults to 200 */
+       /* status: 200, /* Defaults to 200 */
         body: cars
     }
 
